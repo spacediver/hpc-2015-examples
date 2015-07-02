06bf1f0299f1e9c30d3aff8e5340e9dbfa4647f4
Pavel Sutyrin <pavel.sutyrin@gmail.com>
2015-07-03 00:04:26 +0300

    added decorator example

--- decorator.py
+++ decorator.py
@@ -4,3 +4,20 @@ def print_args(fn):
     return fn(*args)
   return printing_args
     
+# Пример использования (декорирования функции)
+def f(x,y):
+  return x+y
+
+# Проверка ниже нужна для того, чтобы печать примеров выполнялась только когда
+# файл decorator.py выполняется как отдельная программа (python3 decorator.py),
+# но не когда импортируется как модуль (import decorator). Во втором случае
+# встроенная переменная __name__ будет равна не '__main__', а 'decorator'.
+if __name__ == '__main__': 
+    print ('f =', f)
+    f_decorated = print_args(f)
+    print ('f_decorated =', f_decorated)
+    print ('------- f(4,5):')
+    print (f(4,5))
+
+    print ('------- f_decorated(4,5):')
+    print (f_decorated(4,5))
